ec5b8a3fb54f1e400e1bb338c4028883d5d89376
nemankyadav <48820915+nemankyadav@users.noreply.github.com>
2019-04-21 00:44:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ var date=new Date()
 var sec=date.getSeconds()
 var min=date.getMinutes()
 var hou=date.getHours()
-var x = document.getElementById("hour");
-x.style.transform="rotate(90deg);"
+
+const x = document.querySelector(".second");
+
+setTimeout(function abcd() {
+    x.style.transform=`rotate(${90+1}deg)`
+    console.log("transformed!")
+}, 1000)
