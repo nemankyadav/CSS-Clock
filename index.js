712d293ee7ed2f03ddf6feb206e6d99b1659b21a
nemankyadav <48820915+nemankyadav@users.noreply.github.com>
2019-04-28 20:45:56 +0530

    Provisional clock with lag (functional)

--- index.js
+++ index.js
@@ -3,10 +3,33 @@ var sec=date.getSeconds()
 var min=date.getMinutes()
 var hou=date.getHours()
 
-const x = document.querySelector(".second");
+var secdeg=90+sec*6
+var mindeg=90+min*6
+var houdeg=90+(hou*30)+(30*min/60)
+// initial time
+const x = document.querySelector(".second")
+const y = document.querySelector(".minute")
+const z = document.querySelector(".hour")
+x.style.transform=`rotate(${secdeg}deg)`
+y.style.transform=`rotate(${mindeg}deg)`
+z.style.transform=`rotate(${houdeg}deg)`
 
+function playClock(){
 setTimeout(function abcd() {
-    x.style.transform=`rotate(${90+1}deg)`
-    console.log("transformed!")
+    secdeg=secdeg+6
+    x.style.transform=`rotate(${secdeg}deg)`
+    console.log("+1sec!")
+        if ((secdeg-90)%360==0) {
+            mindeg=mindeg+6
+                if ((mindeg-90)%12==0) {
+                    houdeg=houdeg+6
+                    z.style.transform=`rotate(${houdeg}deg)`
+                    console.log("+1hou!");
+                }
+            y.style.transform=`rotate(${mindeg}deg)`
+            console.log("+1min!")
+                
+        }
+    playClock();
 }, 1000)
-// Hello
+}
