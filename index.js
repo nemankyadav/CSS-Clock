db4c1390237f4ff78d76bc07879a250066a4857c
nemankyadav <48820915+nemankyadav@users.noreply.github.com>
2019-05-06 16:22:30 +0530

    Completed the sound mute and unmute function

--- index.js
+++ index.js
@@ -7,7 +7,22 @@ function playClock(){
     date=new Date
     x.style.transform=`rotate(${90+date.getSeconds()*6}deg)`
     y.style.transform=`rotate(${90+date.getMinutes()*6}deg)`
-    z.style.transform=`rotate(${90+(date.getHours()*30)+(30*date.getMinutes/60)}deg)`
+    z.style.transform=`rotate(${90+(date.getHours()*30)+(30*date.getMinutes()/60)}deg)`
     sound.play();
 }
-setInterval(playClock, 1000);
+a=setInterval(playClock, 1000);
+
+function playClock0(){
+    date=new Date
+    x.style.transform=`rotate(${90+date.getSeconds()*6}deg)`
+    y.style.transform=`rotate(${90+date.getMinutes()*6}deg)`
+    z.style.transform=`rotate(${90+(date.getHours()*30)+(30*date.getMinutes()/60)}deg)`
+}
+
+flag=0;
+function soundButton() {
+    flag++
+    if(flag%2==1){clearInterval(a);b=setInterval(playClock0, 1000)}
+    if(flag%2==0){clearInterval(b);a=setInterval(playClock, 1000)}
+}
+
